3918fceeb27996ef32182fab1479c838ab3d764a
AlexMurcia <amurciaqu25bj@ikzubirimanteo.com>
2025-10-27 13:57:31 +0100

    triangulo creado

--- Assets/Scripts/Persona.cs
+++ Assets/Scripts/Persona.cs
@@ -5,7 +5,7 @@ public class Persona : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Persona script has started.");
     }
 
     // Update is called once per frame
